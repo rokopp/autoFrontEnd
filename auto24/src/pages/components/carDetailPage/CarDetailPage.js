620f02632e56b4b524eafbb58bec15f13663ce7b
Robin-Kevin Koppa <rokopp@ttu.ee>
2020-10-14 19:10:34 +0300

    Generated pages for individual ads

--- auto24/src/pages/components/carDetailPage/CarDetailPage.js
+++ auto24/src/pages/components/carDetailPage/CarDetailPage.js
@@ -1,15 +1,46 @@
 import React from "react";
+import axios from "axios";
+import MediaCard from "../cards/MediaCards";
+import {Grid} from "@material-ui/core";
+
 
 
 export class CarDetailPage extends React.Component {
     constructor() {
         super();
+        this.state = {
+            carsList: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('https://db8ddf1b-ac50-4227-8b9d-3addfcafe81d.mock.pstmn.io/adsCar')
+            .then(response => {
+                this.setState({carsList: response.data})
+            })
+            .catch(error => {
+                console.log(error)
+            });
     }
 
     render() {
+
+        const { carsList } = this.state;
+        const checkID = this.props.match.params.carID;
         return (
             <div>
-                Car Detail Page
+                {carsList.map(function (item, index) {
+                    if (item.id.toString() === checkID) {
+                        return <Grid item xs={22} sm={14}>
+                            <MediaCard
+                                carID={item.id}
+                                price={item.price}
+                                carMark={item.carMark.carMark}
+                                pictureDto={item.pictureDto}
+                            />
+                        </Grid>;
+                    }
+                })}
             </div>
         )
     }
