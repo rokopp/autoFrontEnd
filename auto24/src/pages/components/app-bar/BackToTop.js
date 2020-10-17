83c522d9ecf54835b05b823b1f39dbf6cb5f2061
Robin-Kevin Koppa <rokopp@ttu.ee>
2020-10-17 23:37:31 +0300

    Tidy up

--- auto24/src/pages/components/app-bar/BackToTop.js
+++ auto24/src/pages/components/app-bar/BackToTop.js
@@ -18,9 +18,6 @@ const useStyles = makeStyles((theme) => ({
 function ScrollTop(props) {
     const { children, window } = props;
     const classes = useStyles();
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
     const trigger = useScrollTrigger({
         target: window ? window() : undefined,
         disableHysteresis: true,
@@ -46,10 +43,6 @@ function ScrollTop(props) {
 
 ScrollTop.propTypes = {
     children: PropTypes.element.isRequired,
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
     window: PropTypes.func,
 };
 
